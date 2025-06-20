c1c47650b1c294c99fd9abfe5cd306b9732958e6
ret2gl1ch <ret2gl1ch@gmail.com>
2025-06-20 23:44:04 +0000

    Add .gitignire for proeject

--- target/debug/deps/vireon_engine-005f7875746b254e.d
+++
@@ -1,5 +0,0 @@
-/home/ret2gl1ch/Dev/vireon/target/debug/deps/libvireon_engine-005f7875746b254e.rmeta: vireon-engine/src/lib.rs
-
-/home/ret2gl1ch/Dev/vireon/target/debug/deps/vireon_engine-005f7875746b254e.d: vireon-engine/src/lib.rs
-
-vireon-engine/src/lib.rs:
--- target/debug/deps/vireon_pam-4e06b653d7b11b27.d
+++
@@ -1,5 +0,0 @@
-/home/ret2gl1ch/Dev/vireon/target/debug/deps/libvireon_pam-4e06b653d7b11b27.rmeta: vireon-pam/src/lib.rs
-
-/home/ret2gl1ch/Dev/vireon/target/debug/deps/vireon_pam-4e06b653d7b11b27.d: vireon-pam/src/lib.rs
-
-vireon-pam/src/lib.rs:
